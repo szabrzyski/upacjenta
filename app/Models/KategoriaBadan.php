e2512915978877e09f939d2627bce44065f74fa1
Sebastian Zabrzyski <szabrzyski@gmail.com>
2022-03-06 15:57:21 +0100

    utworzenie modeli

---
+++ app/Models/KategoriaBadan.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriaBadan extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['*'];
+
+    protected $table = 'kategorie_badan';
+
+    public function badania()
+    {
+        return $this->belongsToMany(Badanie::class, 'badanie_kategoria_badan', 'kategoria_badan_id', 'badanie_id');
+    }
+
+}
